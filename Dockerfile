c16bf80ca898b750c0425b7b6f118aacb3408dc0
Kasumi Hanazuki <kasumi@rollingapple.net>
2018-08-06 09:59:17 +0900

    Dockerfile

---
+++ Dockerfile
@@ -0,0 +1,19 @@
+FROM ubuntu:bionic
+
+RUN apt-get update -qq && \
+    DEBIAN_FRONTEND=noninteractive apt-get install -y --no-install-recommends \
+        cups printer-driver-cups-pdf && \
+    apt-get clean && find /var/lib/apt/lists -type f -delete
+
+COPY cupsd.conf cups-files.conf /etc/cups/
+
+RUN cupsd -f & pid=$! && \
+    while test ! -S /run/cups/cups.sock; do sleep 1; done && \
+    lpadmin -p PDF -v cups-pdf:/ -m lsb/usr/cups-pdf/CUPS-PDF_opt.ppd -E && \
+    while kill "$pid" 2>/dev/null; do sleep 1; done
+
+COPY docker-entrypoint.sh /
+RUN chmod +x docker-entrypoint.sh
+
+EXPOSE 631
+ENTRYPOINT ["/docker-entrypoint.sh"]
---
+++ docker-entrypoint.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+set -eu
+
+: ${CUPS_ADMIN_USERNAME:=admin}
+: ${CUPS_LOG_LEVEL:=warn}
+
+if [[ -z $CUPS_ADMIN_PASSWORD ]]; then
+    echo 'Empty $CUPS_ADMIN_PASSWORD.' >&2
+    exit 1
+fi
+
+if ! id "$CUPS_ADMIN_USERNAME" >/dev/null 2>&1; then
+    useradd -m -G lpadmin -s /usr/sbin/nologin "$CUPS_ADMIN_USERNAME"
+    echo "$CUPS_ADMIN_USERNAME:$CUPS_ADMIN_PASSWORD" | chpasswd
+fi
+
+sed -i /etc/cups/cupsd.conf -e "s|^LogLevel .*$|LogLevel $CUPS_LOG_LEVEL|"
+chmod o+wt /var/spool/cups-pdf/ANONYMOUS
+
+if [[ -d /docker-entrypoint.d ]]; then
+    run-parts --exit-on-error /docker-entrypoint.d
+fi
+
+if [[ ${1-} == /* ]]; then
+    exec "$@"
+fi
+
+exec cupsd -f "$@"
